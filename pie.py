375fd7f4961ee6c0d7ee9aa50c34262ef558a478
princyCodes <princy.9824@gmail.com>
2025-09-29 11:33:40 -0700

    Update pie.py

--- pie.py
+++ pie.py
@@ -9,8 +9,9 @@ known_genres_df = spotify_clean[spotify_clean["Genres"] != "Unknown"]
 
 known_genres_df = known_genres_df["Genres"].value_counts().head(10)  # top 10 genres
 
-plt.figure(figsize=(6,6))
+plt.figure(figsize=(12,6))
 known_genres_df.plot(kind="pie", autopct="%1.1f%%", startangle=140, cmap="tab20")
 plt.title("Genre Distribution")
 plt.ylabel("")  # hide y-label
 plt.show()
+
